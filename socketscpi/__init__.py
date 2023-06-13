6cb0c694a0d2d3e1ae550241feb2b3749ddd1314
Morgan Allison <morgan.allison@keysight.com>
2023-06-13 15:51:22 -0700

    Updated documentation.

--- socketscpi/__init__.py
+++ socketscpi/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Morgan Allison"""
 __email__ = 'morgan.allison@keysight.com'
-__version__ = '2023.04.0'
+__version__ = '2023.06.0'
 
 # For ease of use
 from socketscpi.socketscpi import *
